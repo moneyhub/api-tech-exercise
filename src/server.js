aac0bdaec531c6dbd8ff5dec6493a4d5f2cc00df
KimieDev <91319757+KimieDev@users.noreply.github.com>
2021-10-07 21:30:43 +0100

    init

--- src/server.js
+++ src/server.js
@@ -5,8 +5,22 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+/**
+ * *. implement GET/uers/{userId}/transactions endpoint
+ - 1. get an access token from the bank
+ - 2. retrieve all the transactions from the bank for a given user
+ - 3. format the transaction according to schema laid out in the swagger documentation 
+  ( check sweggar documentation)
+ - update docs for the route implemented (swagger)
+ - make effective use of git
+ */
+
+
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/users/:userId/transactions', (req, res) => res.sendStatus(501));
+
+
+app.use('/users/:userId/transactions', (req, res) => res.sendStatus(200));
 
 module.exports = app;
